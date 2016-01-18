85ab7a118120e8ea2196c774214e6fa40489a6ce
jonoma <jono.matusky@gmail.com>
2016-01-18 17:06:17 -0500

    First commit

---
+++ ImageDisplayer/ImageDisplayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDisplayer
+//
+//  Created by Jonathan Matusky on 1/18/16.
+//  Copyright © 2016 Jonathan Matusky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
